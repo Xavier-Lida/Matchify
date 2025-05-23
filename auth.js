b7b60bcd89de7694a7e3fb74bd1e6402812ec921
Xavier Lida <lidaxavier2005@gmail.com>
2025-05-23 13:35:32 -0400

    Google Auth v2

--- auth.js
+++ auth.js
@@ -1,11 +1,6 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
-//import { MongoDBAdapter } from "@auth/mongodb-adapter";
-//import clientPromise from "./libs/mongo";
 
-/*const config = {
+export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Google],
-  adapter: MongoDBAdapter(clientPromise),
-};*/
-
-export const { handlers, signIn, signOut, auth } = NextAuth({ providers: [Google] });
+});
