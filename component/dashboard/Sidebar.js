0a079a8931b52239a597f23f90cebea7cbd3cf4c
Xavier Lida <lidaxavier2005@gmail.com>
2025-06-29 13:41:28 -0400

    Design sidebar

--- component/dashboard/Sidebar.js
+++ component/dashboard/Sidebar.js
@@ -17,7 +17,13 @@ export default function Sidebar({
       style={{ top: "4rem", left: 0 }}
     >
       <h2 className="text-xl font-bold mb-4">Équipes</h2>
-      <ul className="space-y-1 flex-1 min-h-0 overflow-y-auto">
+      <ul
+        className="space-y-1 flex-1 min-h-0"
+        style={{
+          maxHeight: teams.length > 10 ? "22rem" : "none", // adjust as needed
+          overflowY: teams.length > 10 ? "auto" : "visible",
+        }}
+      >
         {teams.map((team) => (
           <li
             key={team._id}
@@ -34,25 +40,13 @@ export default function Sidebar({
           </li>
         ))}
       </ul>
-      <button
-        className="btn btn-warning mt-4 mb-3"
-        style={{ marginTop: "auto" }}
-        onClick={onGenerateSchedule}
-      >
+      <button className="btn btn-warning mt-4" onClick={onGenerateSchedule}>
         Générer horaire
       </button>
-      <button
-        className="btn btn-success mt-4 mb-3"
-        style={{ marginTop: "auto" }}
-        onClick={onEnterResult}
-      >
+      <button className="btn btn-success mt-4" onClick={onEnterResult}>
         Entrer un match
       </button>
-      <button
-        className="btn btn-primary mt-4"
-        style={{ marginTop: "auto" }}
-        onClick={onAddTeam}
-      >
+      <button className="btn btn-primary mt-4" onClick={onAddTeam}>
         Ajouter une équipe
       </button>
     </aside>
