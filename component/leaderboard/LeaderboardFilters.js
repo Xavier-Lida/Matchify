1f819efe132f39ddcf8af0f113053da1daecca3d
Xavier Lida <lidaxavier2005@gmail.com>
2025-07-13 13:15:51 -0400

    Leaderboard divisions in order

--- component/leaderboard/LeaderboardFilters.js
+++ component/leaderboard/LeaderboardFilters.js
@@ -1,18 +1,29 @@
 export default function LeaderboardFilters({ filters = [], onFilterChange }) {
   return (
     <div className="flex gap-3">
-      {filters.map((filter) => (
-        <select
-          key={filter.name}
-          className="bg-gray-100 border border-gray-300 rounded-md px-3 py-2 text-base focus:outline-none focus:ring-2 focus:ring-blue-400"
-          value={filter.value}
-          onChange={e => onFilterChange(filter.name, e.target.value)}
-        >
-          {filter.options.map(opt => (
-            <option key={opt.value} value={opt.value}>{opt.label}</option>
-          ))}
-        </select>
-      ))}
+      {filters.map((filter) => {
+        // If this is the division filter, sort options numerically
+        const options =
+          filter.name === "division"
+            ? [...filter.options].sort(
+                (a, b) => Number(a.value) - Number(b.value)
+              )
+            : filter.options;
+        return (
+          <select
+            key={filter.name}
+            className="bg-gray-100 border border-gray-300 rounded-md px-3 py-2 text-base focus:outline-none focus:ring-2 focus:ring-blue-400"
+            value={filter.value}
+            onChange={(e) => onFilterChange(filter.name, e.target.value)}
+          >
+            {options.map((opt) => (
+              <option key={opt.value} value={opt.value}>
+                {opt.label}
+              </option>
+            ))}
+          </select>
+        );
+      })}
     </div>
   );
 }
