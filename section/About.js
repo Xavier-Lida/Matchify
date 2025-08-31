41b77298f517083c46b8953a401d92882d19dfbe
Xavier Lida <lidaxavier2005@gmail.com>
2025-08-31 17:00:45 -0400

    Home page small UI update

--- section/About.js
+++ section/About.js
@@ -1,16 +1,11 @@
 // Section.js
 export default function About() {
   return (
-    <section className="pt-40 pb-15 flex flex-col items-center gap-8 bg-base-200">
+    <section className="pt-40 pb-5 flex flex-col items-center gap-8 bg-base-200">
       <div className="w-full">
         <h1 className="text-6xl font-bold mb-6 text-center">
           Ligue Futsal Mauricie
         </h1>
-        <div className="flex flex-col md:flex-row justify-center items-center gap-8">
-          <p className="text-xl mb-0">18 équipes</p>
-          <p className="text-xl mb-0">3 divisions</p>
-          <p className="text-xl mb-0">Dimanche de 18h à 22h</p>
-        </div>
       </div>
     </section>
   );
